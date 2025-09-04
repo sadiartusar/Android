067880019cc973bae211afa347db156df8255cd2
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-09-04 13:22:33 +0600

    add

--- SadiarApp/app/src/main/java/com/example/sadiarapp/Home.java
+++ SadiarApp/app/src/main/java/com/example/sadiarapp/Home.java
@@ -1,6 +1,9 @@
 package com.example.sadiarapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
 
+    private Button btnSubject, btnRound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnSubject=findViewById(R.id.btnSubject);
+        btnRound=findViewById(R.id.btnRound);
+
+        btnSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(), Subject.class);
+                startActivity(intent);
+            }
         });
+
+        btnRound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(), Round.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
